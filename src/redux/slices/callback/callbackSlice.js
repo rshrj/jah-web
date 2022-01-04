d838a1c793016556f688d612652d78b4dc8a9299
Bimalesh Seth <sethbimalesh@users.noreply.github.com>
2022-01-04 18:30:29 +0530

    Callback request state changed added

--- src/redux/slices/callback/callbackSlice.js
+++ src/redux/slices/callback/callbackSlice.js
@@ -1,11 +1,16 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 import { addToast, createFormErrors } from '../errors/errorsSlice';
-
+import { clearTopLoader, setTopLoader } from '../misc/miscSlice';
 import callbackService from '../../../services/callbackService';
+import { arrayToObject } from '../../../utils/helpers';
 
 const initialState = {
-  loading: 'init'
+  loading: 'init',
+  content:{
+    ids:[],
+    callbackrequests:{}
+  }
 };
 
 const submitCallBackRequest = createAsyncThunk(
@@ -19,6 +24,57 @@ const submitCallBackRequest = createAsyncThunk(
       );
 
       dispatch(addToast({ type: 'success', message: data.message }));
+    } catch (error) {
+      if (error.cause.toasts !== undefined && error.cause.toasts.length > 0) {
+        error.cause.toasts.forEach((toastMessage) =>
+          dispatch(addToast({ type: 'error', message: toastMessage }))
+        );
+      }
+      if (error.cause.errors !== undefined || error.cause.errors !== {}) {
+        dispatch(createFormErrors(error.cause.errors));
+      }
+
+      return Promise.reject(error);
+    }
+  }
+);
+
+
+
+const fetchCallBackRequests = createAsyncThunk(
+  'callback/fetchCallBackRequests',
+  async (arg, { dispatch }) => {
+    dispatch(setTopLoader());
+    try {
+      const data = await callbackService.fetchCallBackRequests();
+      dispatch(clearTopLoader());
+      return data.payload;
+    } catch (error) {
+      if (error.cause.toasts !== undefined && error.cause.toasts.length > 0) {
+        error.cause.toasts.forEach((toastMessage) =>
+          dispatch(addToast({ type: 'error', message: toastMessage }))
+        );
+      }
+      if (error.cause.errors !== undefined || error.cause.errors !== {}) {
+        dispatch(createFormErrors(error.cause.errors));
+      }
+
+      return Promise.reject(error);
+    }
+  }
+);
+
+const updateState = createAsyncThunk(
+  'callback/updateState',
+  async ({callbackId, state}, { dispatch }) => {
+    try {
+     
+      const data = await callbackService.updateState(callbackId, state);
+      if(data.message){
+        dispatch(addToast({ type: 'success', message: data.message }));
+      }
+      dispatch(fetchCallBackRequests());
+      
     } catch (error) {
       if (error.cause.toasts !== undefined && error.cause.toasts.length > 0) {
         error.cause.toasts.forEach((toastMessage) =>
@@ -48,9 +104,29 @@ export const callbackSlice = createSlice({
     builder.addCase(submitCallBackRequest.rejected, (state, action) => {
       state.loading = 'idle';
     });
+    builder.addCase(fetchCallBackRequests.pending, (state, action) => {
+      state.loading = 'loading';
+    });
+    builder.addCase(fetchCallBackRequests.fulfilled, (state, action) => {
+      state.loading = 'idle';
+         state.content.callbackrequests = arrayToObject('_id', action.payload);
+         state.content.ids = action.payload.map((r) => r._id);
+    });
+    builder.addCase(fetchCallBackRequests.rejected, (state, action) => {
+      state.loading = 'idle';
+    });
+    builder.addCase(updateState.pending, (state, action) => {
+      state.loading = 'loading';
+    });
+    builder.addCase(updateState.fulfilled, (state, action) => {
+      state.loading = 'idle';
+    });
+    builder.addCase(updateState.rejected, (state, action) => {
+      state.loading = 'idle';
+    });
   }
 });
 
-export { submitCallBackRequest };
+export { submitCallBackRequest, fetchCallBackRequests, updateState };
 
 export default callbackSlice.reducer;
--- src/services/callbackService.js
+++ src/services/callbackService.js
@@ -40,6 +40,88 @@ export const submitCallBackRequest = async (name, phone, message) => {
   }
 };
 
-const callbackService = { submitCallBackRequest };
+export const updateState = async (callbackId, state) => {
+  let token = localStorage.getItem('token');
+  if (!token) {
+    return rejectWithToast('Not authorized to perform this action');
+  }
+
+  try {
+    const res = await fetch(`${apiUrl}/callbackrequests/updateState`, {
+      method: 'PUT',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        callbackId: callbackId,
+        state: state,
+      }),
+    });
+
+    if (!res) {
+      throw errorWithToast('Server did not respond');
+    }
+
+    const data = await res.json();
+    console.log(data);
+
+    if (!res.ok) {
+      throw new Error('Request error', { cause: data });
+    }
+
+    return data;
+  } catch (e) {
+    if (e instanceof TypeError && e.message === 'Failed to fetch') {
+      return rejectWithToast('Server is offline');
+    }
+    return Promise.reject(e);
+  }
+};
+
+export const fetchCallBackRequests = async (name, phone, message) => {
+  let token = localStorage.getItem('token');
+  if (!token) {
+    return rejectWithToast('Not authorized to perform this action');
+  }
+
+  try {
+    const res = await fetch(`${apiUrl}/callbackrequests/all`, {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!res) {
+      throw errorWithToast('Server did not respond');
+    }
+
+    const data = await res.json();
+    console.log(data);
+
+    if (!res.ok) {
+      throw new Error('Request error', { cause: data });
+    }
+
+    return data;
+  } catch (e) {
+    if (e instanceof TypeError && e.message === 'Failed to fetch') {
+      return rejectWithToast('Server is offline');
+    }
+    return Promise.reject(e);
+  }
+};
+
+const callbackService = {
+  submitCallBackRequest,
+  fetchCallBackRequests,
+  updateState,
+};
 
 export default callbackService;
