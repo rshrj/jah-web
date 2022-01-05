87e900347d6abde9594f6c2f896012c7a5c88ace
Rishi Raj <rishiraj.1012exp@gmail.com>
2022-01-05 21:14:38 +0530

    testing GHA #4

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const App = () => {
 
   // To trigger GH Actions
   useEffect(() => {
-    console.log('hello');
+    console.log('hello 2');
   }, []);
 
   const toasts = useSelector((state) => state.errors.toastErrors);
